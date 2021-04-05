a130cedd3b4a6b286a37f3334efc881b9b26b576
elephantcastle <obymik89@gmail.com>
2021-04-05 19:02:15 +0200

    cleanup, reorganise files, Reader component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Reader from './../Reader/Reader';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Reader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Reader/Reader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Reader extends React.Component {
+
+  render() {
+    return (
+      <div aria-label="csv-component">
+          hello
+      </div>
+    );
+  }
+}
+
+export default Reader;
