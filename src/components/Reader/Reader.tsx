76ae98692dae84c34f0e8d986b55906bcf110edf
elephantcastle <obymik89@gmail.com>
2021-04-05 19:12:34 +0200

    setup reader components, axios library, connection working with backedn on /validate-csv

--- src/components/Reader/Reader.tsx
+++ src/components/Reader/Reader.tsx
@@ -1,11 +1,43 @@
 import React from "react";
+import axios from "axios";
 
-class Reader extends React.Component {
+class Reader extends React.Component<{}, { message: Array<string> }> {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = { message: [] };
+  }
+  handleFileUpload = (e: any) => {
+    if (window.FileReader) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      if (file) {
+        reader.onload = (e) => {
+          axios
+            .post("http://localhost:5000/validate-csv", {
+              csvData: reader.result,
+            })
+            .then((response) => {
+              this.setState({ message: response.data });
+            })
+            .catch((e) => {
+             console.log(e)
+            });
+        };
 
+        reader.readAsText(file);
+      }
+    }
+  };
   render() {
     return (
       <div aria-label="csv-component">
-          hello
+        <input
+          type="file"
+          name="csv-input"
+          aria-label="csv-input"
+          onChange={(e) => this.handleFileUpload(e)}
+          accept=".csv"
+        />
       </div>
     );
   }
